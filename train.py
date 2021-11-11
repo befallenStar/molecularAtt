8ace0490076ffd4386a067e8c83b9889730741e4
Junenatte-and-little-pig <sy5622_5@126.com>
2021-11-11 09:48:33 +0800

    init train

--- train.py
+++ train.py
@@ -1,1 +1,17 @@
 # -*- encoding: utf-8 -*-
+"""
+main function to train the network with qm9 dataset
+"""
+import torch
+
+
+def train():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
