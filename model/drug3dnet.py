d6b6224a3353697379d983fd74e53fab7bcfcedd
Junenatte-and-little-pig <sy5622_5@126.com>
2021-11-11 11:30:34 +0800

    add drug3d-net

--- model/drug3dnet.py
+++ model/drug3dnet.py
@@ -1,1 +1,22 @@
 # -*- encoding: utf-8 -*-
+"""
+pytorch version of Drug3D-Net
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from channelAtt import ChannelAtt
+from spatialAtt import SpatialAtt
+
+
+class Drug3DNet(nn.Module):
+    def __init__(self):
+        super(Drug3DNet, self).__init__()
+        # max pooling
+        # convolution + bn + sigmoid
+        # attention
+        # dropout
+
+    def forward(self, x):
+        return x
