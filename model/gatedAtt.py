32f422c4a1957cc04eaae6dd856e2f6ef27e30e2
Junenatte-and-little-pig <sy5622_5@126.com>
2021-11-12 11:16:41 +0800

    gatedAtt module done

--- model/gatedAtt.py
+++ model/gatedAtt.py
@@ -5,15 +5,32 @@ combine SpatialAtt and ChannelAtt with a computed ratio called gate in [Li et al
 """
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 from model.channelAtt import ChannelAtt
 from model.spatialAtt import SpatialAtt
 
 
 class GatedAtt(nn.Module):
-    def __init__(self):
+    def __init__(self, cin, ratio=8):
         super(GatedAtt, self).__init__()
+        self.channel_attention = ChannelAtt(cin, ratio)
+        self.spatial_attention = SpatialAtt()
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        return x
+        channel_feature = self.channel_attention(x)
+        spatial_feature = self.spatial_attention(x)
+        alpha = self.sigmoid(channel_feature + spatial_feature)
+        out = torch.multiply(channel_feature, alpha) + torch.multiply(spatial_feature, 1 - alpha)
+        return out
+
+
+def main():
+    gatedAtt = GatedAtt(32, 8)
+    input = torch.randn([8, 24, 24, 24, 32])
+    out = gatedAtt(input)
+    print(out.shape)
+
+
+if __name__ == '__main__':
+    main()
