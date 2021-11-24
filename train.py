c98d4420c3f8493acdef6d15fbb1961e4af07904
Junenatte-and-little-pig <sy5622_5@126.com>
2021-11-24 16:55:54 +0800

    modify the dataloading for the large scale dataset

--- train.py
+++ train.py
@@ -2,16 +2,17 @@
 """
 main function to train the network with qm9 dataset
 """
+import os
+
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, random_split
 
-from ignite.handlers import EarlyStopping
 from model.drug3dnet import Drug3DNet
 from utils.loadData import load_data
 
 
-def train(model, train_db, epoch, batch_size, optimizer=None, val_fn=None):
+def train(model, train_db, batch_size, optimizer=None, val_fn=None):
     """
     main function to train the data with cross validation
     :param model: the model to be trained
@@ -30,28 +31,25 @@ def train(model, train_db, epoch, batch_size, optimizer=None, val_fn=None):
     train_db, val_db = random_split(train_db, [split_ratio, len(train_db) - split_ratio])
     train_loader = DataLoader(train_db, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_db, batch_size=batch_size, shuffle=True)
-    for i in range(epoch):
-        for batch_idx, (data, target) in enumerate(train_loader):
-            pred = model(data)
-            loss = val_fn(pred, target)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            # print period result
-            if batch_idx % 1 == 0:
-                print("Epoch :{} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(i, batch_idx * len(data), len(train_loader.dataset),
-                                                                         100. * batch_idx / len(train_loader), loss.item()))
+    for batch_idx, (data, target) in enumerate(train_loader):
+        pred = model(data)
+        loss = val_fn(pred, target)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # print period result
+        if batch_idx % 1 == 0:
+            print("[{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(batch_idx * len(data), len(train_loader.dataset), 100. * batch_idx / len(train_loader), loss.item()))
 
-        # compute the loss of validation
-        mae_loss = 0
-        for batch_idx, (data, target) in enumerate(val_loader):
-            pred = model(data)
-            val_loss = val_fn(target, pred)
-            mae_loss += val_loss
-        mae_loss /= len(val_loader.dataset)
-        print("Validation MSE Loss: {:.6f}".format(mae_loss))
+    # compute the loss of validation
+    mae_loss = 0
+    for batch_idx, (data, target) in enumerate(val_loader):
+        pred = model(data)
+        val_loss = val_fn(target, pred)
+        mae_loss += val_loss
+    mae_loss /= len(val_loader.dataset)
 
-        # do the early stopping
+    print("Validation MSE Loss: {:.6f}".format(mae_loss))
 
     return model
 
@@ -76,16 +74,28 @@ def test(model, test_db, batch_size, val_fn):
 def main():
     model = Drug3DNet(5)
     dir_path = './data/input32'
-    # read from ./data/input/train
-    train_db = load_data(dir_path, mode='train')
-    # read from ./data/input/test
-    test_db = load_data(dir_path, mode='test')
+    train_path = os.path.join(dir_path, 'train')
+    test_path = os.path.join(dir_path, 'test')
     epoch = 10
     batch_size = 8
-    # learning rate
-    learning_rate = 0.01
-    train(model, train_db, epoch, batch_size, optimizer=optim.Adadelta(model.parameters(), lr=learning_rate), val_fn=nn.L1Loss())
-    test(model, test_db, batch_size, val_fn=nn.L1Loss())
+    for e in range(epoch):
+        print("Epoch: {}".format(e))
+        for _, dirs, _ in os.walk(train_path):
+            for dir in dirs:
+                subpath = os.path.join(train_path, dir)
+                # read from ./data/input/train
+                train_db = load_data(subpath)
+                # read from ./data/input/test
+                # learning rate
+                learning_rate = 0.01
+                train(model, train_db, batch_size, optimizer=optim.Adadelta(model.parameters(), lr=learning_rate), val_fn=nn.L1Loss())
+
+    for _, dirs, _ in os.walk(test_path):
+        for dir in dirs:
+            subpath = os.path.join(test_path, dir)
+            # read from ./data/input/test
+            test_db = load_data(subpath)
+            test(model, test_db, batch_size, val_fn=nn.L1Loss())
 
 
 if __name__ == '__main__':
