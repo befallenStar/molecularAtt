b1c3169b15a4d02fc347707c696ee52e150b6d65
Junenatte-and-little-pig <sy5622_5@126.com>
2022-01-10 04:00:35 +0800

    add parameter for spatial only network

--- model/gatedSCT.py
+++ model/gatedSCT.py
@@ -11,7 +11,7 @@ from model.swinTransformer import SwinTransformer
 
 
 class GatedSCT(nn.Module):
-    def __init__(self, cin, size, num_heads, window_size):
+    def __init__(self, cin, size, num_heads, window_size, gated=True):
         super(GatedSCT, self).__init__()
         self.spatial = nn.Sequential(
             # W-MSA
@@ -19,14 +19,19 @@ class GatedSCT(nn.Module):
             # SW-MSA
             SwinTransformer(cin, size, num_heads, window_size=window_size, shift_size=window_size // 2)
         )
-        self.channel = GCT(cin)
+        self.channel = None
+        if gated:
+            self.channel = GCT(cin)
         self.tanh = nn.Tanh()
 
     def forward(self, x):
         spatial = self.spatial(x)
-        channel = self.channel(x)
-        alpha = self.tanh(spatial + channel)
-        out = torch.multiply(spatial, alpha) + torch.multiply(channel, alpha)
+        if self.channel:
+            channel = self.channel(x)
+            alpha = self.tanh(spatial + channel)
+            out = torch.multiply(spatial, alpha) + torch.multiply(channel, alpha)
+        else:
+            out = spatial
         out += x
         return out
 
@@ -34,7 +39,7 @@ class GatedSCT(nn.Module):
 def main():
     num_heads = 5
     window_size = 8
-    gatedSCT = GatedSCT(5, 16, num_heads, window_size)
+    gatedSCT = GatedSCT(5, 16, num_heads, window_size, False)
     data = torch.randn([4, 16, 16, 16, 5])
     result = gatedSCT(data)
     print(result.shape)
